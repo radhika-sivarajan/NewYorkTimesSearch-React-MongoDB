1edb9bfc5899497ddcf73d528b52ba47ee2f0eae
Radhika Sivarajan <radhikaraj333@gmail.com>
2017-05-29 19:26:59 -0700

    axios used for API call

--- app/nytAPI.js
+++ app/nytAPI.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var axios = require('axios');
 
 var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
 
@@ -6,11 +6,11 @@ var nytHelper = {
     searchArticle: function (topic, startYear, endYear) {
         var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
             authKey + "&q=" + topic + "&begin_date=" + startYear + "0101" + "&end_date=" + endYear + "0101";
-        request(queryURL, function (error, response, body) {
-            if (JSON.parse(body).response.docs.length === 0) {
+        return axios.get(queryURL).then(function (results) {
+            if (results.data.response.docs.length === 0) {
                 console.log("No records");
             } else {
-                var NYTRecords = JSON.parse(body).response;
+                var NYTRecords = results.data.response;
                 var NYTResultArray = [];
                 for (var i = 0; i < NYTRecords.docs.length; i++) {
                     if (NYTRecords.docs[i].headline.main) {
@@ -24,6 +24,7 @@ var nytHelper = {
                     }
                 }
                 console.log(NYTResultArray);
+                return NYTResultArray;
             }
         });
     }
